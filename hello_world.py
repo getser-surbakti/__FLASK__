df114bc1f07e8af31ff33606e8d1bfedec812ab2
Getser Surbakti, B.Sc. <getser845@gmail.com>
2024-12-05 11:39:11 +0800

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World, Welcome to My Page"
+
+@app.route('/about')
+def about():
+    return "The is the About Me"
+
+if __name__ == '__main__':
+    app.run(debug=True)

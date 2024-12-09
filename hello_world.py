c898d7561d9f3c11613c8b7bbb3fe1f5051eaee6
getser-surbakti <getser845@gmail.com>
2024-12-09 12:18:48 +0800

    resolved again

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World People, Welcome to My Page"
+    return "Hello World People People, Welcome to My Page"
 
 @app.route('/about')
 def about():
